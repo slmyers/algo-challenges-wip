1c631366e040530f7cb6df227f8d3ae7bb8a3115
Steven Myers <slmyers@ualberta.ca>
2020-03-06 17:54:45 -0700

    Update RunningMedian.js

--- RunningMedian.js
+++ RunningMedian.js
@@ -1,5 +1,110 @@
 // https://www.hackerrank.com/challenges/find-the-running-median/problem
 
+// using kth smallest quickselect where k is median index
+function computeMedian(values) {
+    if (values.length === 0) return 0
+
+    const half = Math.floor(values.length / 2);
+
+    if (values.length % 2)
+        return values[half];
+
+    return (values[half - 1] + values[half]) / 2.0;
+}
+
+function swap(a, i, j) {
+    let t = a[i]
+    a[i] = a[j]
+    a[j] = t 
+}
+
+function partition(arr, left, right, pivot) {
+    let pivotValue = arr[pivot]
+    let lesserThanTailIndex = left
+
+    // console.log("------")
+    // console.log("before clear")
+    // console.log(arr)
+    // clear the pivot
+    swap(arr, pivot, right)
+    // console.log("after clear")
+    // console.log(arr)
+
+    // bulldoze through sort space
+    for (let i = left; i < right; i++) {
+        if (arr[i] < pivotValue) {
+            swap(arr, i, lesserThanTailIndex)
+            lesserThanTailIndex++
+        }
+    } 
+    // console.log("bulldozed")
+    // console.log(arr)
+    // // restore the pivot
+    // console.log("restored")
+    swap(arr, right, lesserThanTailIndex)
+    // console.log(arr)
+    console.log("-----")
+
+    return lesserThanTailIndex
+}
+
+function medianSelect(arr, k) {
+    if (arr.length <= 1) return arr[0]
+
+    let n = arr.length
+    let i = 0
+    let j = n - 1
+    const input = [...arr]
+
+    while (i <= j) {
+        let chosenPivotIndex = getRandomInt(j - i) + i
+        // console.log(arr)
+        // console.log("chosenPivotIndex: " + chosenPivotIndex)
+        // console.log("j: " + j)
+        // console.log("i: " + i)
+        // console.log("length: " + arr.length)
+        let actualPivotIndex = partition(arr, i, j, chosenPivotIndex)
+        // console.log("actual pivot index: " + actualPivotIndex + `, k: ${k}`)
+        if (actualPivotIndex === n - k) {
+            return arr[actualPivotIndex]
+        } else if (actualPivotIndex > n - k) {
+            // overshot
+            j = actualPivotIndex - 1
+        } else {
+            i = actualPivotIndex + 1
+        }
+    }
+}
+
+/*
+ * Complete the runningMedian function below.
+ */
+function runningMedian(a) {
+    let result = []
+    let current = []
+    /*
+     * Write your code here.
+     */
+    while(a.length) {
+        current.push(a.shift())
+        const half = Math.floor(current.length / 2);
+
+        if (current.length < 25 && 1 === 4 /* do not enter */) {
+            // simpler to just calculate
+            result.push(Number(computeMedian(current)).toFixed(1))
+        } else {
+            // TODO: this seems to calculate the correct median for odd sized arrays
+            medianSelect(current, half + 1)
+        }
+    }
+    return result
+}
+
+// END quickselect
+
+
+
+
 // super dumb version
 
 // I think it O(n * m * log(m))?
@@ -32,87 +137,3 @@ function runningMedian(a) {
 }
 
 // end super dumb
-
-
-
-
-
-
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(str => str.trim());
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-function medianOfMedians(n) {
-
-}
-
-function computeMedian(arr) {
-    let result = null
-
-    arr.sort()
-    const length = arr.length
-    const mid = length / 2
-    if (length % 2 !== 0) {
-        result = arr[Math.floor(mid)]
-    } else {
-        result = (arr[Math.floor(mid) - 1] + arr[Math.floor(mid)]) / 2
-    }
-
-    return result
-}
-
-/*
- * Complete the runningMedian function below.
- */
-function runningMedian(a) {
-    let result = []
-    let current = []
-    /*
-     * Write your code here.
-     */
-    while(a.length) {
-        current.push(a.shift())
-        computeMedian(current)
-    }
-    
-    return result.map(n => Number(n).toFixed(1))
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const aCount = parseInt(readLine(), 10);
-
-    let a = [];
-
-    for (let aItr = 0; aItr < aCount; aItr++) {
-        const aItem = parseInt(readLine(), 10);
-        a.push(aItem);
-    }
-
-    let result = runningMedian(a);
-
-    ws.write(result.join("\n") + "\n");
-
-    ws.end();
-}
